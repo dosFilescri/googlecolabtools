70aaf8ef42be140719a25292e4432033dfd23b84
dosFilescri <112200360+dosFilescri@users.noreply.github.com>
2022-08-28 13:12:04 +0300

    Create cmd.py

---
+++ cmd.py
@@ -0,0 +1,9 @@
+"""
+This a command line for your Google Colab
+Dev: DFCR
+"""
+print('Code by maxliban__')
+print('Warning! Don't save here any files. After restart all files will deleted')
+import os
+a= input('>')
+os.system(a)
